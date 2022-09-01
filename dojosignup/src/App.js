5dd50779b63329f412ac40db68cdd7e5866a474a
SimonHls <simon.huelskamp@gmail.com>
2022-09-01 11:57:04 -0600

    changed auth behavior

--- dojosignup/src/App.js
+++ dojosignup/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Landingpage from './pages/Landingpage'
 import {
   RecoilRoot
@@ -13,34 +13,76 @@ import ManageDojo from './pages/ManageDojo';
 import { ApplicationControl } from './ApplicationControl';
 import DojoSignupPage from './pages/DojoSignupPage';
 import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
+
+  const [userIsSignedIn, setUserIsSignedIn] = useState(false)
+
+  //Verify login status. Only logged in users can see certain pages
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserIsSignedIn(true);
+    } else {
+      setUserIsSignedIn(false);
+    }
+  });
+
   return (
     <RecoilRoot>
       <BrowserRouter>
         <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
           <ApplicationControl>
-            <Routes>
 
-              <Route path='/' element={
-                <Login />
-              }/>
 
-              <Route path='/home' element={
-                <Landingpage />
-              }/>
+              {userIsSignedIn ? (
 
-              <Route path='/dojosignup' element={
-                <div className='flex justify-center relative'>
-                  <DojoSignupPage />
-                </div>
-              }/>
+              //Check if user is logged in
+              <Routes>
 
-              <Route path='/managedojo' element={
-                <ManageDojo />
-              }/>
+                <Route path='/' element={
+                  <Landingpage />
+                }/>
 
-            </Routes>
+                <Route path='/home' element={
+                  <Landingpage />
+                }/>
+
+                <Route path='/dojosignup' element={
+                  <div className='flex justify-center relative'>
+                    <DojoSignupPage />
+                  </div>
+                }/>
+
+                <Route path='/managedojo' element={
+                  <ManageDojo />
+                }/>
+
+                <Route path='*' element={
+                  <PageNotFound />
+                }/>
+              </Routes>
+
+              ) : (
+
+              // => User is not logged in
+              <Routes>
+                <Route path='/' element={
+                  <Login />
+                }/>
+                <Route path='/home' element={
+                  <Login />
+                }/>
+                <Route path='*' element={
+                  <PageNotFound />
+                }/>
+              </Routes>
+
+              )}
+
+
           </ApplicationControl>
         </div>
       </BrowserRouter>
